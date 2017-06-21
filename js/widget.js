c8bbffb41a52a62848b1f943bd529b1340106aa9
Walter Kachanovsky <walterkachanovsky@Walters-MacBook-Pro.local>
2017-06-21 15:07:23 +0300

    changed src links back to local for testing

--- js/widget.js
+++ js/widget.js
@@ -1,7 +1,7 @@
 (function () {
     //Load Stylesheet
-    // var root = './';
-    var root = 'https://rawgit.com/kachanovskyi/toyotacr-widget/master/';
+    var root = './';
+    // var root = 'https://rawgit.com/kachanovskyi/toyotacr-widget/master/';
     // var accessToken = "afc2e32efdff44819a7cbc62e58009ca";
     // var baseUrl = "https://api.api.ai/v1/";
 
