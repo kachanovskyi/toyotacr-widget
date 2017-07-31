301d7d02b07ff208c67f19da1808fd283ec83e85
Walter Kachanovsky <walterkachanovsky@Walters-MacBook-Pro.local>
2017-07-31 13:41:53 +0300

    fixing mobile styles

--- js/widget.js
+++ js/widget.js
@@ -242,6 +242,7 @@
             $("#chatInput").val('');
 
             if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+                alert('!!!');
                 $("html, body")
                     .animate({
                         scrollTop: 0
